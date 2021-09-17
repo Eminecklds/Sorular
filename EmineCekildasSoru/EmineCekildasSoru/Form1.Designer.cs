1f48293eedffaf0b1b57e267821368dd2178734c
Eminecklds <42403779+Eminecklds@users.noreply.github.com>
2021-09-17 10:19:35 +0300

    Add files via upload

---
+++ EmineCekildasSoru/EmineCekildasSoru/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace EmineCekildasSoru
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.snc = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(113, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sayı Üret";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // snc
+            // 
+            this.snc.AutoSize = true;
+            this.snc.Location = new System.Drawing.Point(182, 174);
+            this.snc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.snc.Name = "snc";
+            this.snc.Size = new System.Drawing.Size(0, 13);
+            this.snc.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2. Soruyu Görüntüle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.snc);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label snc;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EmineCekildasSoru/EmineCekildasSoru/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmineCekildasSoru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static readonly ThreadLocal<Random> rnd
+     = new ThreadLocal<Random>(() => new Random());
+        HashSet<int> numbers = new HashSet<int>();
+        int count,index;
+        string mesaj;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mesaj = "";
+            count = 0;
+            index = 0;
+            while (index!=9)
+            {
+                numbers.Add( rnd.Value.Next(1, 10));
+                index = numbers.Count;
+                count++;
+            }
+
+            foreach (int item in numbers)
+            { 
+                mesaj+=item+",";
+            }
+            snc.Text = mesaj + " Random fonksiyonu " + count + " kere çalışmıştır.";
+            numbers.Clear();
+        }
+    }
+}
